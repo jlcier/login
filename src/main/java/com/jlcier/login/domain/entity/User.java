21faeff7b67ed6923f603c3ffc059675f59b4f9e
jlcier <145858443+jlcier@users.noreply.github.com>
2025-02-02 21:49:44 -0300

    interface UserDetails

--- src/main/java/com/jlcier/login/domain/entity/User.java
+++ src/main/java/com/jlcier/login/domain/entity/User.java
@@ -3,16 +3,25 @@ package com.jlcier.login.domain.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "User")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 }
